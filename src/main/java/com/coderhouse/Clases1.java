cf71ab262c2ff68e7decf69bf4e99b9fb9959348
Acer <rominaeguzman@gmail.com>
2024-11-21 20:00:37 -0300

    commit4

--- src/main/java/com/coderhouse/Clases1.java
+++ src/main/java/com/coderhouse/Clases1.java
@@ -3,7 +3,67 @@ package com.coderhouse;
 public class Clases1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+			// 1- variable entera
+				byte edadDelUsuario = 37;
+				//2- Constante para días de la semana
+				final byte DIAS_DE_LA_SEMANA = 7;
+				//3- Double para el precio de un producto
+				double precioDelProducto = 597.52d;
+				//4- Boolean para aprobado y desaprobado
+				boolean aprobado = true;
+				boolean desaprobado = false;
+				//5- Char para inicial
+				char inicialNombre = 'E';
+				//6- arreglo de entero de notas para 5 estudiantes
+				int [] notasEstudiantes = new int [5];
+				notasEstudiantes[0] = 2;
+				notasEstudiantes[1] = 5;
+		        notasEstudiantes[2] = 8;
+		        notasEstudiantes[3] = 4;
+				notasEstudiantes[4] = 10;
+				//7- String para nombre completo
+		        string nombreDePErsona = "Romina Guzmán"
+				//8- peso de una persona 
+				float pesoPersona = 65.8f;
+				//9- Constante para pi
+				final float VALOR_DE_PI = 3.14f;
+				//10- Constante mayor cantidad de estudiantes
+				final byte MAXIMO_ESTUDIANTES = 16;
+				//11- Suma de dos enteros
+				int numero1 = 450;
+				int numero2 = 550;
+				System.out.println(numero1 + numero2);
+				// Otra opción
+		        int numeroA = 450;
+				int numeroB = 550;
+				int resultado2 = numeroA + numeroB;
+				System.out.println(resultado);
+
+				//12- Resta con double
+				double numero3 = 45.5d;
+			    double numero4 = 55.5d;
+				double resultado3 = numero3 - numero4;
+				System.out.println(resultado);
+				//13 Muliplicación
+
+				int num1 = 6;
+				int num2 = 4;
+				System.out.println(num1 * num2);
+				//14- 
+
+				//15- Modulo
+				int num8 = 15;
+				int num9 = 2;
+				int sobrante = num8 % num9;
+				System.out.println("El resto de la operación es + sobrante");
+				
+				//16- Comparación número mayor
+
+				int numA = 16;
+				int numB = 54;
+
+				System.out.println(numA > numB);
+		
 
 	}
 
